92a4ef80dc9625ac44a0c0d576157e2cf9e257ed
Borja Esteban <boressan@outlook.com>
2024-07-30 10:31:35 +0200

    fix database sandbox with experiments and groups

--- sandbox/load-database.sh
+++ sandbox/load-database.sh
@@ -2,10 +2,25 @@
 
 mongoimport --jsonArray \
     --db test-data \
-    --collection app.blueprints.drift \
-    --file tmp/database/test-drifts.json
+    --collection app.experiments \
+    --file tmp/database/test-experiments.json
+
+mongoimport --jsonArray \
+    --db test-data \
+    --collection app.groups \
+    --file tmp/database/test-groups.json
 
 mongoimport --jsonArray \
     --db test-data \
     --collection app.blueprints.user \
     --file tmp/database/test-users.json
+
+mongoimport --jsonArray \
+    --db test-data \
+    --collection app.00000000-0000-0001-0001-000000000001 \
+    --file tmp/database/test-01-drifts.json
+
+mongoimport --jsonArray \
+    --db test-data \
+    --collection app.00000000-0000-0001-0001-000000000002 \
+    --file tmp/database/test-02-drifts.json
