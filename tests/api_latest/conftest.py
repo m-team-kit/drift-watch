2b60d7263cc2359c1707dd0452657c3a23bcc70b
Borja Esteban <boressan@outlook.com>
2024-12-13 11:22:31 +0100

    fix: use correct path on api integration tests

--- tests/api_latest/conftest.py
+++ tests/api_latest/conftest.py
@@ -8,7 +8,7 @@ from pytest import fixture
 def path(request):
     """Return the path for the request."""
     if not hasattr(request, "param"):
-        return "api/latest/experiment"
+        return "api/latest/experiment/search"
     if request.param is None:
         return None
     return request.param
